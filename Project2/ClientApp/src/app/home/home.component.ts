a5f89eccd3933f770128e86520289b1999ece754
anthivrousi <anthivrousi@gmail.com>
2023-01-23 17:12:00 +0200

    sign in form

--- Project2/ClientApp/src/app/home/home.component.ts
+++ Project2/ClientApp/src/app/home/home.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  username = new FormControl('');
-  email = new FormControl('');
+export class HomeComponent implements OnInit{
+  signInForm!: FormGroup;
+  username = new FormControl('', [Validators.required]);
+  email = new FormControl('', [Validators.required, Validators.email]);
 
+  ngOnInit() {
+    this.signInForm = new FormGroup({
+      username: this.username,
+      email: this.email
+    });
+  }
 
+  onSubmit() {
+    console.log(this.signInForm.value);
+  }
 
+  getErrorMessage() {
+    if (this.username.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 }
