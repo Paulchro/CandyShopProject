6bb60875036727de0a2ab7241e002991284c5980
anthivrousi <vrousi.anthi@nbg.gr>
2023-03-20 12:14:32 +0200

    dynamic cart form

--- Project2/ClientApp/src/app/form-cart/form-cart.component.ts
+++ Project2/ClientApp/src/app/form-cart/form-cart.component.ts
@@ -1,8 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormControl, Validators, FormBuilder, NgForm } from '@angular/forms';
-import {ThemePalette} from '@angular/material/core';
-import {FloatLabelType} from '@angular/material/form-field';
-import {MatRadioModule} from '@angular/material/radio';
+import { FormControl, Validators, FormBuilder, NgForm, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-cart',
@@ -10,35 +8,76 @@ import {MatRadioModule} from '@angular/material/radio';
   styleUrls: ['./form-cart.component.css']
 })
 export class FormCartComponent {
-  email = new FormControl('', [Validators.required, Validators.email]);
+  public formData: any;
+  public cartForm?: any;
 
-  name = new FormControl('');
+  constructor(private httpClient: HttpClient){
+  }
 
-  colorControl = new FormControl('primary' as ThemePalette);
+  ngOnInit(){
+    this.cartForm = this.getFormControlsFields();
+  }
 
-  hideRequiredControl = new FormControl(false);
+  onSubmit(){}
 
-  floatLabelControl = new FormControl('auto' as FloatLabelType);
+  getFormControlsFields() {
+    let form:any = {};
+    this.httpClient.get('/assets/json_forms/forms.json').subscribe((formData: any) =>{
+        this.formData = formData.controlscart;
+        
+        for (let i=0; i<this.formData.length;i++) {
+          const validators = this.addValidator(this.formData[i].validators);
+          form[this.formData[i].name] = new FormControl(this.formData[i].value || '', validators);
+        }    
+      })
+    this.cartForm = new FormGroup(form);
+    return this.cartForm;
+  }
 
-  showname: string = '';
-
-  options = this._formBuilder.group({
-    hideRequired: this.hideRequiredControl,
-    floatLabel: this.floatLabelControl,
-  });
-
-
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
+  private addValidator(validators: any) {
+    if (!validators) {
+      return [];
     }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  
-}
-constructor(private _formBuilder: FormBuilder) {}
-
-  getFloatLabelValue(): FloatLabelType {
-    return this.floatLabelControl.value || 'auto';
+    const validatorsN = Object.keys(validators).map((rulvalidator): any => {
+      switch (rulvalidator) {
+        case "required":
+          return Validators.required;    
+        case "minLength":
+          return Validators.minLength(10);
+      }
+    });
+    return validatorsN;
   }
+//   email = new FormControl('', [Validators.required, Validators.email]);
+
+//   name = new FormControl('');
+
+//   colorControl = new FormControl('primary' as ThemePalette);
+
+//   hideRequiredControl = new FormControl(false);
+
+//   floatLabelControl = new FormControl('auto' as FloatLabelType);
+
+//   showname: string = '';
+
+//   options = this._formBuilder.group({
+//     hideRequired: this.hideRequiredControl,
+//     floatLabel: this.floatLabelControl,
+//   });
+
+
+//   getErrorMessage() {
+//     if (this.email.hasError('required')) {
+//       return 'You must enter a value';
+//     }
+
+//     return this.email.hasError('email') ? 'Not a valid email' : '';
+  
+// }
+// constructor(private _formBuilder: FormBuilder) {}
+
+//   getFloatLabelValue(): FloatLabelType {
+//     return this.floatLabelControl.value || 'auto';
+//   }
 }
